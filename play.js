61a3dca4ad0e15256d31b400d746e5168fc8b299
Tamara Tell <2telltamara@gmail.com>
2023-05-10 17:32:33 -0400

    add client.js to modularize play.js

--- play.js
+++ play.js
@@ -1,25 +1,3 @@
-const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  }
-  );
-
-  conn.setEncoding('utf-8');
-
-  console.log("Connecting...");
-
-  conn.on('connect', () => {
-    console.log('Connected to server!');
-  });
-
-  conn.on('data', (data) => {
-    console.log(`Received message: ${data}`);
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 connect();
